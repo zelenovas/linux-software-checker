cd6f40f503bf30295d589a87f0b0f84ee1b75fed
Alex Z <zelenovas@gmail.com>
2019-09-13 00:11:21 +0300

    Реализовал проверку версий. Другой синтаксис файла конфигурации.

--- softchecker.sh
+++ softchecker.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
-
 # Software Checker Functions ==========>
 
 # return 1 if global command line program installed, else 0
 # example
 # echo "node: $(program_is_installed node)"
-function program_is_installed {
+function program_is_installed() {
   # set to 1 initially
   local return_=1
   # set to 0 if not found
@@ -16,10 +14,20 @@ function program_is_installed {
   echo "$return_"
 }
 
+function program_is_correct() {
+  local return_=0
+
+  if $1 --version | grep -q $2; then
+    local return_=1
+  fi
+
+  echo "$return_"
+}
+
 # return 1 if local npm package is installed at ./node_modules, else 0
 # example
 # echo "gruntacular : $(npm_package_is_installed gruntacular)"
-function npm_package_is_installed {
+function npm_package_is_installed() {
   # set to 1 initially
   local return_=1
   # set to 0 if not found
@@ -30,10 +38,10 @@ function npm_package_is_installed {
 
 # display a message in red with a cross by it
 # example
-# echo echo_fail "No"
-function echo_fail {
+# echo echo_fail "No"98:1
+function echo_fail() {
   # echo first argument in red
-  printf "\e[31mNOT FOUND ${1}"
+  printf "\e[31m${2} ${1}"
   # reset colours back to normal
   printf "\e[0m"
 }
@@ -41,9 +49,9 @@ function echo_fail {
 # display a message in green with a tick by it
 # example
 # echo echo_fail "Yes"
-function echo_pass {
+function echo_pass() {
   # echo first argument in green
-  printf "\e[32mINSTALLED ${1}"
+  printf "\e[32m${2} ${1}"
   # reset colours back to normal
   printf "\e[0m"
 }
@@ -52,54 +60,57 @@ function echo_pass {
 # example
 # echo echo_if 1 "Passed"
 # echo echo_if 0 "Failed"
-function echo_if {
+function echo_if() {
   if [ $1 == 1 ]; then
     echo_pass $2
   else
-    echo_fail $2
+    echo_fail $3
   fi
 }
 
 # <========== Software Checker Functions
 
+if [ -z "$1" ]; then
+  echo "Config filename required! Usage: ./softwareChecker.sh /path/to/file.conf"
+else
+  CONFIG_FILEPATH=$1
+  #echo $CONFIG_FILEPATH
 
+  die() {
+    printf >&2 "%s\n" "$@"
+    exit 1
+  }
 
+  appname=''
+  linenb=0
+  while read line; do
+    ((++linenb))
 
+    #       if [[ $line =~ ^[[:space:]]*$ ]]; then
+    #          continue
 
+    regexpAppname="^\s*([[:alnum:]]+):(.*)$"
+    if [[ $line =~ $regexpAppname ]]; then
+      appname=${BASH_REMATCH[1]}
+      appversion=${BASH_REMATCH[2]}
+      #declare -A $appname
+      is_installed=$(program_is_installed $appname)
 
-if [ -z "$1" ]
-  then
-    echo "Config filename required! Usage: ./softwareChecker.sh /path/to/file.conf"
-  else
-    CONFIG_FILEPATH=$1
-    #echo $CONFIG_FILEPATH
+      if [ $is_installed == 1 ]; then
+          echo "$appname $appversion $(echo_if $(program_is_correct $appname $appversion) 'CORRECT' 'WRONG')"
+      else
+          echo "$appname $(echo_if $is_installed 'INSTALLED' 'NOT_INSTALLED')"
+      fi
 
-    die() {
-       printf >&2 "%s\n" "$@"
-       exit 1
-    }
+    elif [[ $line =~ ^([^=]+)=(.*)$ ]]; then
+      [[ -n $appname ]] || die "*** Error line $linenb: no array name defined"
+      printf -v ${appname}["${BASH_REMATCH[1]}"] "%s" "${BASH_REMATCH[2]}"
 
-    appname=''
-    linenb=0
-    while read line; do
-       ((++linenb))
-       if [[ $line =~ ^[[:space:]]*$ ]]; then
-          continue
-       elif [[ $line =~ ^\[([[:alpha:]][[:alnum:]]*)\]$ ]]; then
-          appname=${BASH_REMATCH[1]}          
-          #declare -A $appname
-          echo "$appname $(echo_if $(program_is_installed $appname))"
-
-       elif [[ $line =~ ^([^=]+)=(.*)$ ]]; then
-          [[ -n appname ]] || die "*** Error line $linenb: no array name defined"
-          printf -v ${appname}["${BASH_REMATCH[1]}"] "%s" "${BASH_REMATCH[2]}"
-       else
-          die "*** Error line $linenb: $line"
-       fi
-    done < "$CONFIG_FILEPATH"
+    else
+      die "*** Error line $linenb: $line"
+    fi
+  done <"$CONFIG_FILEPATH"
 fi
-
-
 
 # command line programs
 #echo "apache2 $(echo_if $(program_is_installed apache2))"
@@ -110,7 +121,6 @@ fi
 #echo "deploid $(echo_if $(program_is_installed deploid))"
 #echo "phing $(echo_if $(program_is_installed phing))"
 #echo "composer $(echo_if $(program_is_installed composer))"
-
 
 # local npm packages
 #echo "lodash  $(echo_if $(npm_package_is_installed lodash))"
